703a1fa4c447aef3bfe6ae0a6ec71b13c106d4b2
Eugene <immer277@gmail.com>
2021-11-12 18:32:36 -0800

    Nov 12 - Eugene
    added show alien, submit button, textbox

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-
     var inputText = document.createElement("input")
-    var fullText = document.createTextNode("inputText")
+    inputText.className = "textbox"
     document.body.appendChild(inputText)
     
     
-
     var submitButton = document.createElement("button")
+    submitButton.className = "submit-button"
+    submitButton.textContent = "Submit"
     document.body.appendChild(submitButton)
+    submitButton.addEventListener("click", checkText)
 
 
-    function showAlien()
+    function checkText()
     {
-    var alien = document.createElement("img")
-    alien.src = "./images/alien/alienlwalk0.gif"
-    document.body.appendChild(alien)
+        if (inputText = 'alien')
+        {
+            //showAlien()
+            console.log('text is alien')
+        }
     }
     
+        function showAlien()
+    {
+        var alien = document.createElement("img")
+        alien.className = "alienImage"
+        alien.src = "./images/alien/alienlwalk0.gif"
+        document.body.appendChild(alien)
+        //This just renders the alien onto the page. 
+    }
 
+    function showStorage()
+    {
+        
+    }
+
+    
 //Index.html should have the alien walking 
-//with a header with the text "Alien"
 //while loop so that while the number of the image is less than 30
 //it starts to loop through the pictures so it makes it look like
 //the alien is walking left or right. 
