d4103e9c0bd2ebd4228dbf9eadc109b60ee9db60
Eugene <immer277@gmail.com>
2021-11-12 16:42:46 -0800

    commit

--- index.js
+++ index.js
@@ -12,4 +12,8 @@
 
 
 
-//Index.html should have the alien walking with a header with the text "Alien"
+//Index.html should have the alien walking 
+//with a header with the text "Alien"
+//while loop so that while the number of the image is less than 30
+//it starts to loop through the pictures so it makes it look like
+//the alien is walking left or right. 
